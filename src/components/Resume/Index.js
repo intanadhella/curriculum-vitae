ba46f9a270d077a8cc5a761e0d20143fbf4a01d2
Intan Adela <intanadhelia655@gmail.com>
2020-11-08 10:54:59 +0700

    Add indentation

--- src/components/Resume/Index.js
+++ src/components/Resume/Index.js
@@ -1,28 +1,27 @@
-import React from "react"
-import About from "./About"
-import WorkExperience from "./WorkExperience"
-import Education from "./Education"
-import Skills from "./Skills"
-import Languages from "./Languages"
-import PersonalProjects from "./PersonalProjects"
+import React from "react";
+import About from "./About";
+import WorkExperience from "./WorkExperience";
+import Education from "./Education";
+import Skills from "./Skills";
+import Languages from "./Languages";
+import PersonalProjects from "./PersonalProjects";
 
 const Resume = () => {
+  return (
+    <div className="container-fluid p-0">
+      <About />
+      <hr className="m-0" />
+      <WorkExperience />
+      <hr className="m-0" />
+      <Education />
+      <hr className="m-0" />
+      <Skills />
+      <hr className="m-0" />
+      <Languages />
+      <hr className="m-0" />
+      <PersonalProjects />
+    </div>
+  );
+};
 
-    return (
-        <div className="container-fluid p-0">
-            <About />
-            <hr className="m-0" />
-            <WorkExperience />
-            <hr className="m-0" />
-            <Education />
-            <hr className="m-0" />
-            <Skills />
-            <hr className="m-0" />
-            <Languages />
-            <hr className="m-0" />
-            <PersonalProjects />
-        </div>
-    )
-}
-
-export default Resume
+export default Resume;
